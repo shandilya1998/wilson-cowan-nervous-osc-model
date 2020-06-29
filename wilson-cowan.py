d4f848faee84cfb752f41be508c42f7c8cd49ec2
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-06-29 17:58:35 +0530

    commit experiment 2 plot

--- wilson-cowan.py
+++ wilson-cowan.py
@@ -26,7 +26,7 @@ class WilsonCowan(object):
         return (self.f(self.mu*(self.a*self.u - self.b*self.v + self.Su)) - self.u)/self.Tu
 
     def dv(self):
-        return (self.f(self.mu*(self.c*self.u - self.d*self.v + self.Sv)) - self.v)/self.Tu
+        return (self.f(self.mu*(self.c*self.u - self.d*self.v + self.Sv)) - self.v)/self.Tv
 
     def simulate(self):
         for i in tqdm(range(self.n)):
@@ -36,9 +36,9 @@ class WilsonCowan(object):
             self.u += self.dt*self.du()
             self.v +=self.dt*self.dv()
 
-osc = WilsonCowan(Tu = 0.2, Tv = 0.2, a = 5.6, d = -2.4, b = 5.6, c = 2.4, Su = 0.02, Sv = 0.02, mu = 1)
+osc = WilsonCowan(Tu = 1, Tv = 0.2, a = 5.6, d = -2.4, b = 5.6, c = 2.4, Su = 0.02, Sv = 0.02, mu = 1)
 osc.simulate()
-fig, axes = plt.subplots(3, 1, figsize = (10, 30))
+fig, axes = plt.subplots(3, 1, figsize = (30, 10))
 axes[0].plot(osc.Au[-10000:])
 axes[0].set_ylabel('excitatory neuron output')
 axes[0].set_xlabel('time')
@@ -48,5 +48,5 @@ axes[1].set_xlabel('time')
 axes[2].plot(osc.Ay[-10000:])
 axes[2].set_ylabel('CPG output')
 axes[2].set_xlabel('time')
-fig.savefig('wilson_cowan_exp1.png')
+fig.savefig('wilson_cowan_exp2.png')
 plt.show()
