13c20765ef54e35f723240bf185d9f973e5c61f5
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-07-01 16:56:23 +0530

    commit quadruped motion joint angles simulation experiment 1

---
+++ cpg_wilson_cowan.py
@@ -0,0 +1,124 @@
+import numpy as np
+from tqdm import tqdm 
+import matplotlib.pyplot as plt
+
+class CPGWilsonCowan(object):
+    def __init__(self, Tu, Tv, a, b, c, d, Su, Sv, mu, p,W, f = np.tanh, num = 4, dt = 0.0001, n = 100000):
+        self.num = num
+        self.Tu = Tu
+        self.Tv = Tv
+        self.a = a 
+        self.b = b 
+        self.c = c 
+        self.d = d 
+        self.Su = Su
+        self.Sv = Sv
+        self.f = f 
+        self.mu = mu
+        self.u = np.random.random((self.num,))
+        self.v = np.random.random((self.num,))
+        self.n = n 
+        self.p = p
+        self.W = W
+        self.Au = np.zeros((self.n, self.num))
+        self.Av = np.zeros((self.n, self.num))
+        self.Ay = np.zeros((self.n, self.num))
+        self.dt = dt 
+        self.t = np.arange(0, self.n)*dt
+
+    def du(self):
+        du = np.zeros((self.num,))
+        for i in range(self.num):
+            temp = 0.0
+            for j in range(self.num):
+                temp += self.W[i][j]*self.u[j]
+            du[i] = (self.f(self.mu[i]*(self.a[i]*self.u[i] - self.b[i]*self.v[i] + temp + self.Su[i])) - self.u[i])/self.Tu[i]
+        return du
+
+    def dv(self):
+        dv = np.zeros((self.num,))
+        for i in range(self.num):
+            temp = 0.0 
+            for j in range(self.num):
+                temp += self.W[i][j]*self.v[j]
+            dv[i] = (self.f(self.mu[i]*(self.c[i]*self.u[i] - self.d[i]*self.v[i] + temp + self.Sv[i])) - self.v[i])/self.Tv[i]
+        return dv
+
+    def simulate(self, disturb_time = None, gait_transition = None ):
+        for i in tqdm(range(self.n)):
+            self.Au[i, :] = self.u
+            self.Av[i, :] = self.v
+            self.Ay[i, :] = self.p*(self.u - self.v)
+            self.u += self.dt*self.du()
+            self.v += self.dt*self.dv()
+            if disturb_time and disturb_time == i:
+                self.set_u(np.random.random((self.num,)))
+                self.set_v(np.random.random((self.num,)))
+            
+            if gait_transition and gait_transition == i:
+                self.W[0][2] = -self.W[0][2]
+                self.W[2][3] = -self.W[2][3]
+                self.W[2][0] = -self.W[2][0]
+                self.W[3][2] = -self.W[3][2]
+
+    def get_u(self):
+        return self.u + self.dt*self.du()
+
+    def get_v(self):
+        return self.v + self.dt*self.dv()     
+
+    def set_u(self, u):
+        self.u = u
+    
+    def set_v(self, v):
+        self.v = v
+
+    def set_W(self, W):
+        self.W = W
+    
+    def plot(self):
+        labels = []
+        fig, axes = plt.subplots(3, 2, figsize=(10, 15))
+        for i in range(self.num):
+            axes[0, 0].plot(self.t[-50000:], self.Au[-50000:, i])
+            axes[0, 1].plot(self.t[:50000], self.Au[:50000, i])
+            labels.append('neuron_'+str(i))
+        axes[0, 0].set_ylabel('u steady')
+        axes[0, 1].set_ylabel('u initial') 
+        axes[0, 0].legend(labels, loc = 'upper left')
+        axes[0, 1].legend(labels, loc = 'upper left')
+        labels = []
+        for i in range(self.num):
+            axes[1, 0].plot(self.t[-50000:], self.Ay[-50000:, i])
+            axes[1, 1].plot(self.t[:50000], self.Av[:50000, i])
+            labels.append('neuron_'+str(i))
+        axes[1, 0].legend(labels, loc = 'upper left')
+        axes[1, 1].legend(labels, loc = 'upper left')
+        axes[1, 0].set_ylabel('v steady')
+        axes[1, 1].set_ylabel('v initial') 
+        labels = []
+        for i in range(self.num):
+            axes[2, 0].plot(self.t[-50000:], self.Ay[-50000:, i])
+            axes[2, 1].plot(self.t[:50000], self.Ay[:50000, i])
+            labels.append('neuron_'+str(i))
+        axes[2, 0].set_ylabel('y steady')
+        axes[2, 1].set_ylabel('y initial') 
+        axes[2, 0].legend(labels, loc = 'upper left')
+        axes[2, 1].legend(labels, loc = 'upper left')
+        fig.savefig('plots/cpg_wilson_cowan_exp2.png')
+
+W = np.full((4,4), -0.1)
+for i in range(4):
+    W[i][i] = 0.0
+Tu = np.full((4,), 0.2)
+Tv = np.full((4,), 0.2)
+a = np.full((4,), 5.6)
+b = np.full((4,), 5.6)
+c = np.full((4,), 2.4)
+d = np.full((4,), -2.4)
+Su = np.full((4,), 0.02)
+Sv = np.full((4,), 0.02)
+mu = np.full((4,), 1)
+osc = CPGWilsonCowan(Tu = Tu, Tv = Tv, a = a, d = d, b = b, c = c, Su = Su, Sv = Sv, mu = mu, p = 0.5, W = W) 
+#osc.simulate(gait_transition = 0)#disturb_time = 5000000)
+#osc.plot()
--- quadruped-cpg.py
+++ quadruped-cpg.py
@@ -1,19 +1,21 @@
 import numpy as np
 from tqdm import tqdm 
-from cpg-wilson-cowan import osc
+from cpg_wilson_cowan import osc
+import matplotlib.pyplot as plt
 
 class QuadCPG(object):
     def __init__(self, alpha_k, alpha_h, base_cpg, A_k, A_h):
         self.alpha_k = alpha_k
         self.alpha_h = alpha_h
-        self.A_k
+        self.A_k = A_k
+        self.A_h = A_h 
         self.base_cpg = base_cpg
         self.u_h = np.random.random((self.base_cpg.num,))
         self.v_h = np.random.random((self.base_cpg.num,))
         self.base_cpg.set_u(self.u_h)
         self.base_cpg.set_v(self.v_h)
         self.y_h = self.base_cpg.p*(self.u_h - self.v_h)
-        self.y_k = np.random.random((self.base_cpg.num,))
+        self.y_k = np.zeros((self.base_cpg.num,))
         self.y_h_dot = np.random.random((self.base_cpg.num,))
         self.Arr_y_h = np.zeros((self.base_cpg.n, self.base_cpg.num))
         self.Arr_y_k = np.zeros((self.base_cpg.n, self.base_cpg.num))      
@@ -22,17 +24,30 @@ class QuadCPG(object):
         return self.base_cpg.p*(self.base_cpg.du()-self.base_cpg.dv())
 
     def simulate(self):
-        for i in range(self.base_cpg.n):
+        for i in tqdm(range(self.base_cpg.n)):
             self.Arr_y_h[i, :] = self.y_h
             self.Arr_y_k[i, :] = self.y_k
             self.y_h_dot = self.get_y_h_dot()
-            if y_h_dot < 0:
-                self.y_k = 0.0
-            else:
-                self.y_k = self.alpha_k*self.A_k*(1-(self.y_h/(self.alpha_h*self.A_h))**2))
+            for j in range(self.base_cpg.num):
+                if self.y_h_dot[j] < 0:
+                    self.y_k[j] = 0.0 #*self.alpha_k*self.A_k*(-(self.y_h[j]/(self.alpha_h*self.A_h))**2)
+                else:
+                    self.y_k[j] = self.alpha_k*self.A_k*(1-(self.y_h[j]/(self.alpha_h*self.A_h))**2)
             self.u_h = self.base_cpg.get_u()
-            self.base_cpg.set_u(self.u_h)
             self.v_h = self.base_cpg.get_v()
+            self.base_cpg.set_u(self.u_h)
             self.base_cpg.set_v(self.v_h)
             self.y_h = self.base_cpg.p*(self.u_h - self.v_h)
-            
+        
+    def plot(self):
+        labels = []
+        fig, axes = plt.subplots(4, 1,  figsize=(5, 20))
+        for i in range(self.base_cpg.num):
+            axes[i].plot(self.base_cpg.t[-50000:], self.Arr_y_h[-50000:, i])
+            axes[i].plot(self.base_cpg.t[-50000:], self.Arr_y_k[-50000:, i])
+        plt.show()
+        fig.savefig('plots/cpg_quad_exp1.png')
+
+cpg = QuadCPG(alpha_k = 1, alpha_h = 0.75, base_cpg = osc, A_h = 1, A_k = 0.91)
+cpg.simulate()
+cpg.plot()
